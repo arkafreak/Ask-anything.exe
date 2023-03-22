1053b8833e051ba88fae853a2ca077a448c652a4
ARKA PRAVO DUTTA <77730711+arkafreak@users.noreply.github.com>
2023-03-22 12:45:03 +0530

    Create just ask.py

---
+++ just ask.py
@@ -0,0 +1,28 @@
+import os
+import csv
+import openai
+openai.api_key = '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'
+model_engine = 'text-davinci-003'
+print("\\\ ENTER WIHOUT TYPING ANYTHING TO EXIT FROM THE TERMINAL //")
+print("\t \t \t \t \\\ ENJOY YOUR TIME HERE //")
+for i in range(1,50):
+	prompt = input(" \t \n Please enter your query : ")
+	#if(prompt == "shutdown"):
+  	#os.system("shutdown /s /t 1")
+	if(len(prompt) >= 1):
+		completion = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1024, n=1, stop=None, temperature=0.5)
+		response = completion.choices[0].text 
+		print(response)
+		with open('INPUT.txt', 'a') as f:
+			f.write("\t  INPUT QUERY : ")
+		with open('INPUT.txt', 'a') as f:
+			f.write(prompt) 
+		with open('OUTPUT.txt', 'a') as f:
+			f.write(response)
+	else:
+		print('\t')
+		print("Sorry to see you go -) ")
+		print('\t')
+		break
+exit()	
+				
